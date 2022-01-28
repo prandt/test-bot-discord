d413c1567e7ffb061aaa5d249b1b81a3581ad237
Richard Prandt <rprandt.contato@gmail.com>
2022-01-28 18:01:01 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	} else if (commandName === 'fuuhz'){
+		await interaction.reply(`${interaction.user.username} joga nada kkkkkkk`)
+		
+	}
+});
+
+client.login(token);
